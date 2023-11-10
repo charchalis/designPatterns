917e7b2d7af637326d434ad804222f6d5b652ea3
charchalis <charchalis97@gmail.com>
2023-11-10 12:02:49 +0000

    finished composite pattern

---
+++ src/main/java/org/example/StringTransformerGroup.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import java.util.List;
+
+public class StringTransformerGroup implements StringTransformer {
+
+    private List<StringTransformer> transformers;
+
+    public StringTransformerGroup(List<StringTransformer> transformers) {
+        this.transformers = transformers;
+    }
+
+    @Override
+    public void execute(StringDrink drink) {
+        for(StringTransformer transformer: transformers){
+            transformer.execute(drink);
+        }
+    }
+}
